ac858fb5d14bcb6bfa2dd4d36733ec2722b78d19
시혁 <shihyuck@imhuichan-ui-MacBookPro.local>
2022-11-29 12:10:41 +0900

    create project

---
+++ TodoShareAPP/TodoShareAPP/TodoShareAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoShareAPPApp.swift
+//  TodoShareAPP
+//
+//  Created by 시혁 on 2022/11/29.
+//
+
+import SwiftUI
+
+@main
+struct TodoShareAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
